da8a073db3bdfc5438098bfebe5dc2bc43620852
Ilia Bukalov <ilyabukalov@gmail.com>
2020-09-26 15:59:42 +0800

    PollForm added

--- vue/src/views/Index.vue
+++ vue/src/views/Index.vue
@@ -5,15 +5,18 @@
       <sp-bank-balances />
       <sp-token-send />
       <!-- this line is used by starport scaffolding # 4 -->
-		<sp-type-form type="poll" :fields="['title', 'options', ]" />
+      <poll-form />
+		<!-- <sp-type-form type="poll" :fields="['title', 'options', ]" /> -->
     </div>
   </div>
 </template>
 
 <script>
 import * as sp from "@tendermint/vue";
+import PollForm from "./../components/PollForm.vue";
+
 
 export default {
-  components: { ...sp },
+  components: { ...sp, PollForm },
 };
 </script>
