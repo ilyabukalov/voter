011ed8c9029e5c203016ee3b689db1e0f31ed2bb
Ilia Bukalov <ilyabukalov@gmail.com>
2020-09-26 16:50:21 +0800

    votes added

--- vue/src/views/Index.vue
+++ vue/src/views/Index.vue
@@ -5,6 +5,7 @@
       <sp-bank-balances />
       <sp-token-send />
       <!-- this line is used by starport scaffolding # 4 -->
+		<sp-type-form type="vote" :fields="['pollID', 'value', ]" />
       <poll-form />
 		<!-- <sp-type-form type="poll" :fields="['title', 'options', ]" /> -->
     </div>
